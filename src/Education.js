4100a0b1c21e3e19637d6c27fe102b0767622d90
RafalZywica <rzywica96@gmail.com>
2024-11-05 17:13:29 +0100

    Adding Contact, Eductation, Home templates

---
+++ src/Education.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Education() {
+    return (
+        <div>
+            <h2>Education Page</h2>
+            <p>Welcome to the Education page!</p>
+        </div>
+    );
+}
