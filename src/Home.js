9452d0ade2b31a8a8ed5e37015d48c7c95276ad0
rafalzywica <rzywica96@gmail.com>
2024-11-08 13:54:23 +0100

    Adjusting Home page

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import './Home.css'
 
 export default function Home() {
     return (
-        <div>
-            <h2>Home Page</h2>
-            <p>Welcome to the Home page!</p>
+        <div className="about">
+            <p>About me:</p>
+            <p>Driven Software Engineer with a strong commitment to excellence in every stage of the development lifecycle. Enthusiastic about learning and contributing to impactful projectsm always aiming to deliver reliable, high-quality software solutions.
+            </p>
         </div>
     );
 }
