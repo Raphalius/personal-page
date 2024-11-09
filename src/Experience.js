47976951ed44ae8238556ace8568168dc6eeec55
rafalzywica <rzywica96@gmail.com>
2024-11-09 13:46:51 +0100

    Adding better interactivity

--- src/Experience.js
+++ src/Experience.js
@@ -3,17 +3,33 @@ import './Experience.css';
 
 export default function Experience() {
     const [selectedProject, setSelectedProject] = useState(null);
+    const [selectedFilter, setSelectedFilter] = useState(null);
+
     const projects = [
         {
             name: 'Project A',
-            description: 'This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different.',
+            description: 'This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different.',
             languages: ['JavaScript', 'Python'],
             frameworks: ['React', 'Django'],
             skills: ['Frontend', 'Backend']
         },
         {
             name: 'Project B',
-            description: 'Description of Project B',
+            description: 'Description of Project B. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different.',
+            languages: ['TypeScript', 'Java'],
+            frameworks: ['Angular', 'Spring Boot'],
+            skills: ['Full Stack', 'API Design']
+        },
+        {
+            name: 'Project C',
+            description: 'Description of Project B. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different.',
+            languages: ['TypeScript', 'Java'],
+            frameworks: ['Angular', 'Spring Boot'],
+            skills: ['Full Stack', 'API Design']
+        },
+        {
+            name: 'Project D',
+            description: 'Description of Project B. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different. This is a short description of Project A. It is here for testing purposes, actual descriptions will be different.',
             languages: ['TypeScript', 'Java'],
             frameworks: ['Angular', 'Spring Boot'],
             skills: ['Full Stack', 'API Design']
@@ -25,26 +41,59 @@ export default function Experience() {
     const allSkills = [...new Set(projects.flatMap(project => project.skills))];
 
     const handleProjectClick = (project) => {
-        setSelectedProject(project);
+        if (selectedProject && selectedProject.name === project.name) {
+            setSelectedProject(null);
+        } else {
+            setSelectedProject(project);
+        }
+        setSelectedFilter(null);
+    };
+
+    const handleFilterClick = (item, category) => {
+        if (selectedFilter && selectedFilter.item === item) {
+            setSelectedFilter(null);
+        } else {
+            setSelectedFilter({ item, category });
+        }
+        setSelectedProject(null);
     };
 
     const getItemStyle = (item, category) => {
         const positive = { opacity: 1, backgroundColor: '#007200', transition: 'background-color 0.5s ease, opacity 0.5s ease' };
         const negative = { opacity: 0.2, backgroundColor: 'gray', transition: 'background-color 0.5s ease, opacity 0.5s ease' };
-        if (!selectedProject || selectedProject[category].includes(item))
+
+        if (selectedProject) {
+            return selectedProject[category].includes(item) ? positive : negative;
+        } else if (selectedFilter) {
+            return selectedFilter.item === item ? positive : negative;
+        } else {
             return positive;
-        return negative;
-    }
+        }
+    };
 
     return (
         <div className="experience-container">
             <div className="left-column">
                 {projects.map((project, index) => (
-                    <div key={index} className="project-item" 
-                    style={{ opacity: selectedProject && selectedProject.name !== project.name ? 0.2 : 1, transition: 'opacity 0.5s ease' }} 
-                    onClick={() => handleProjectClick(project)}>
+                    <div
+                        key={index}
+                        className="project-item"
+                        style={{
+                            opacity:
+                                selectedProject && selectedProject.name === project.name
+                                    ? 1
+                                    : selectedProject
+                                        ? 0.2
+                                        : selectedFilter &&
+                                            !project[selectedFilter.category].includes(selectedFilter.item)
+                                            ? 0.2
+                                            : 1,
+                            transition: 'opacity 0.5s ease',
+                        }}
+                        onClick={() => handleProjectClick(project)}
+                    >
                         <h3>{project.name}</h3>
-                        <p className='small-font'>{project.description}</p>
+                        <p className="small-font">{project.description}</p>
                     </div>
                 ))}
             </div>
@@ -53,8 +102,14 @@ export default function Experience() {
                     <h4>Languages:</h4>
                     <div className="list-container">
                         {allLanguages.map((language, idx) => (
-                            <div key={idx} className="list-item small-font"
-                                style={getItemStyle(language, 'languages')}>{language}</div>
+                            <div
+                                key={idx}
+                                className="list-item small-font"
+                                style={getItemStyle(language, 'languages')}
+                                onClick={() => handleFilterClick(language, 'languages')}
+                            >
+                                {language}
+                            </div>
                         ))}
                     </div>
                 </div>
@@ -62,8 +117,14 @@ export default function Experience() {
                     <h4>Frameworks:</h4>
                     <div className="list-container">
                         {allFrameworks.map((framework, idx) => (
-                            <div key={idx} className="list-item small-font"
-                                style={getItemStyle(framework, 'frameworks')}>{framework}</div>
+                            <div
+                                key={idx}
+                                className="list-item small-font"
+                                style={getItemStyle(framework, 'frameworks')}
+                                onClick={() => handleFilterClick(framework, 'frameworks')}
+                            >
+                                {framework}
+                            </div>
                         ))}
                     </div>
                 </div>
@@ -71,8 +132,14 @@ export default function Experience() {
                     <h4>Skills:</h4>
                     <div className="list-container">
                         {allSkills.map((skill, idx) => (
-                            <div key={idx} className="list-item small-font"
-                                style={getItemStyle(skill, 'skills')}>{skill}</div>
+                            <div
+                                key={idx}
+                                className="list-item small-font"
+                                style={getItemStyle(skill, 'skills')}
+                                onClick={() => handleFilterClick(skill, 'skills')}
+                            >
+                                {skill}
+                            </div>
                         ))}
                     </div>
                 </div>
